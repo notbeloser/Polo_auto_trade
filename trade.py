8bf29dbea1d3bd3d1b24f0a97740d03681d5c1db
notbeloser <a850710a@gmail.com>
2017-06-26 11:39:29 +0800

    creating html

--- trade.py
+++ trade.py
@@ -1,8 +1,7 @@
-import poloniex
-import time
+from poloniex import Poloniex
+from time import time
 import sys
 import numpy as np
-import datetime as dt
 import matplotlib.dates as md
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,11 +9,14 @@ import matplotlib.finance as mpf
 from matplotlib.dates import DateFormatter, WeekdayLocator,DayLocator, MONDAY
 from colorama import Fore, Back, Style
 from datetime import datetime
+from math import pi
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
 
 pd.set_option('display.width', 200)
 #print(Fore.RED + 'some red text'+Style.RESET_ALL)
-polo = poloniex.Poloniex('GBC146G1-M9RGA0VT-T5FL729B-P8OTN6SU',
-'a4d44e8e4e4432e9a9a94d66fb17a7b7081858aaeb85c0fdd9b6ebf8a51a7d2fa0160c5db0e55b8d836ba6d64b1c0e324eba164b94278617edd2eec48c09acb7')
+polo = Poloniex('GBC146G1-M9RGA0VT-T5FL729B-P8OTN6SU',
+'a4d44e8e4e4432e9a9a94d66fb17a7b7081858aaeb85c0fdd9b6ebf8a51a7d2fa0160c5db0e55b8d836ba6d64b1c0e324eba164b94278617edd2eec48c09acb7',jsonNums=float)
 
 coin_pair=['BTC_ETH','BTC_XRP','BTC_LTC','BTC_ZEC','BTC_ETC','BTC_DGB','BTC_BTS','BTC_LBC','BTC_FCT','BTC_ARDR','BTC_STRAT','BTC_NXT','BTC_STR','BTC_DASH'
 ,'BTC_LSK','BTC_SC','BTC_XEM','BTC_STEEM','BTC_GNT','BTC_XMR','BTC_DOGE','BTC_POT','BTC_SYS','BTC_MAID','BTC_GAME','BTC_BURST','BTC_BCN','BTC_REP','BTC_DCR'
@@ -22,29 +24,35 @@ coin_pair=['BTC_ETH','BTC_XRP','BTC_LTC','BTC_ZEC','BTC_ETC','BTC_DGB','BTC_BTS'
 'BTC_XCP','BTC_XBC','BTC_VRC','BTC_RIC','BTC_PASC','BTC_BTCD','BTC_EXP','BTC_SBD','BTC_SJCX','BTC_NEOS','BTC_FLO','BTC_BELA','BTC_NAUT','BTC_XPM','BTC_NMC',
 'BTC_BCY','BTC_XVC','BTC_BTM','BTC_HUC']
 k_line = 50
-chart = [pd.DataFrame()]*len(coin_pair)
+period = polo.MINUTE * 5
+df = [pd.DataFrame()]*len(coin_pair)
 # for i in range(len(coin_pair)):
-for i in range(1):
+for i in range(2):
     # print(coin_pair[i])
-    chart[i]=pd.DataFrame(polo.returnChartData(coin_pair[i], 300, time.time() - 300 * k_line, time.time()))
-    # print(chart[i])
-    quotes_str=['date','open','close','high','low']
-    quote = pd.DataFrame.transpose(chart[i][quotes_str[:]]).convert_objects(convert_numeric=True).as_matrix()
-    date = ([dt.datetime.fromtimestamp(quote[0][a]) for a in range(len(quote[0]))])
-    quote[0] = md.date2num(date)
-    mondays = WeekdayLocator(MONDAY)  # major ticks on the mondays
-    alldays = DayLocator()  # minor ticks on the days
-    weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
-    dayFormatter = DateFormatter('%d')  # e.g., 12
+    df[i]=pd.DataFrame(polo.returnChartData(coin_pair[i],period,time()-polo.DAY))
+    df[i]['date'] = pd.to_datetime(df[i]["date"], unit='s')
+    df[i].dropna(inplace=True)
+    w = (period * 1000) - 5000
+    tools = "pan,wheel_zoom,box_zoom,reset,save"
 
-    fig, ax = plt.subplots()
-    fig.subplots_adjust(bottom=0.2)
-    ax.xaxis.set_major_locator(mondays)
-    ax.xaxis.set_minor_locator(alldays)
-    ax.xaxis.set_major_formatter(weekFormatter)
-    mpf.candlestick_ochl(ax, quote, width=0.6)
+    output_file(coin_pair[i]+".html", title=coin_pair[i]+"-Poloniex")
 
-    plt.show()
+    p = figure(x_axis_type="datetime", tools=tools, plot_width=1500, title=coin_pair[i])
+    p.xaxis.major_label_orientation = pi / 4
+    p.grid.grid_line_alpha = 0.7
+
+    inc = df[i].close > df[i].open
+    dec = df[i].open > df[i].close
+
+    p.segment(df[i].date, df[i].high, df[i].date, df[i].low, color="black")
+
+    p.vbar(df[i].date[inc], w, df[i].open[inc], df[i].close[inc], fill_color="green", line_color="black")
+    p.vbar(df[i].date[dec], w, df[i].open[dec], df[i].close[dec], fill_color="red", line_color="black")
+
+    show(p)
+
+
+
 
 
 
