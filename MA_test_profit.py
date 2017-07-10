a80120fe5358a6fa018099c3c31d244bef323ba8
notbeloser <a850710a@gmail.com>
2017-07-10 13:11:25 +0800

    Add Maid trade

--- MA_test_profit.py
+++ MA_test_profit.py
@@ -22,31 +22,30 @@ def print_full(x):
 pd.set_option('display.width', 300)
 polo = Poloniex('GBC146G1-M9RGA0VT-T5FL729B-P8OTN6SU',
 'a4d44e8e4e4432e9a9a94d66fb17a7b7081858aaeb85c0fdd9b6ebf8a51a7d2fa0160c5db0e55b8d836ba6d64b1c0e324eba164b94278617edd2eec48c09acb7',jsonNums=float)
-coin = "BTC_DASH"
+coin = "BTC_MAID"
 
 period = polo.MINUTE * 5
 output_file(coin+".html", title="Poloniex-即時k線")
 
 
+
+
 window_short = 8
 window_long = 6
-SDP = 0.11
-SDN = -0.73
-
-
+SDP = 0.262626
+SDN= -0.232323
+df=pd.DataFrame(polo.returnChartData(coin,period,time()-polo.DAY*365*2))
+index = 0
 print(coin)
-
-df=pd.DataFrame(polo.returnChartData(coin,period,time()-polo.DAY*2))
-df['date'] = df['date']+polo.DAY/3  #shift time to UTC+8
+df['date'] = df['date'] + polo.DAY / 3  # shift time to UTC+8
 df['date'] = pd.to_datetime(df["date"], unit='s')
-
-
-df['short'] = pd.ewma(df['close'],com= window_short )
+df['short'] = pd.ewma(df['close'], com=window_short)
 df['long'] = pd.rolling_mean(df['close'], window=window_long)
-df['short_diff'] = df['short'].diff() /df['short'] *100
-df['long_diff'] = df['long'].diff() / df['long']*100
-df['SD'] = (df.short - df.long)/df.long * 100
+df['short_diff'] = df['short'].diff() / df['short'] * 100
+df['long_diff'] = df['long'].diff() / df['long'] * 100
+df['SD'] = (df.short - df.long) / df.long * 100
 df['SD_diff'] = df['SD'].diff()
+
 df['buy'] = df.SD > SDP
 df['sell'] = df.SD < SDN
 df['bs'] = df.buy != df.sell
@@ -56,32 +55,32 @@ df.dropna(inplace=True)
 df['trade'] = pd.DataFrame.diff(df.buy[trade_index]*1 + df.sell[trade_index]*-1)
 df['trade'].fillna(0,inplace=True)
 df=df.drop(['buy','sell','bs'],axis=1)
-print_full(df)
-
-w = (period * 1000) - 5000
-tools = "pan,wheel_zoom,box_zoom,reset,save,hover"
-
-p = figure(x_axis_type="datetime", tools=tools, plot_width=1200,plot_height=650, title=coin)
-p.xaxis.major_label_orientation = pi / 4
-p.grid.grid_line_alpha = 2
-inc = df.close > df.open
-dec = df.open > df.close
-p.segment(df.date, df.high, df.date, df.low, color="black")
-
-p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="green", line_color="black")
-p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="red", line_color="black")
-
-p.line(df.date,df.short,color='yellow')
-p.line(df.date,df.long,color='blue')
-
-
-
-trade_index = df[df['trade'] ==2].index.tolist()
-p.circle(df['date'][trade_index], df['trade'][trade_index]/2*df['weightedAverage'][trade_index]*0.01 +df['weightedAverage'][trade_index] , color='blue')
-trade_index = df[df['trade'] ==-2].index.tolist()
-p.circle(df['date'][trade_index],
-            df['trade'][trade_index] / 2 * df['weightedAverage'][trade_index] * 0.01 +
-            df['weightedAverage'][trade_index], color='black')
+# print_full(df)
+#
+# w = (period * 1000) - 5000
+# tools = "pan,wheel_zoom,box_zoom,reset,save,hover"
+#
+# p = figure(x_axis_type="datetime", tools=tools, plot_width=1200,plot_height=650, title=coin)
+# p.xaxis.major_label_orientation = pi / 4
+# p.grid.grid_line_alpha = 2
+# inc = df.close > df.open
+# dec = df.open > df.close
+# p.segment(df.date, df.high, df.date, df.low, color="black")
+#
+# p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="green", line_color="black")
+# p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="red", line_color="black")
+#
+# p.line(df.date,df.short,color='yellow')
+# p.line(df.date,df.long,color='blue')
+#
+#
+#
+# trade_index = df[df['trade'] ==2].index.tolist()
+# p.circle(df['date'][trade_index], df['trade'][trade_index]/2*df['weightedAverage'][trade_index]*0.01 +df['weightedAverage'][trade_index] , color='blue')
+# trade_index = df[df['trade'] ==-2].index.tolist()
+# p.circle(df['date'][trade_index],
+#             df['trade'][trade_index] / 2 * df['weightedAverage'][trade_index] * 0.01 +
+#             df['weightedAverage'][trade_index], color='black')
 
 df_index=df.index.tolist()
 
@@ -116,6 +115,7 @@ for i in df_index:
         last_price = df['close'][i]
         trade_time=trade_time+1
 
+win_rate = win/trade_time *100
+print("BTC %f fee %f trade time %d win %d lose %d,win rate %f,SDN %f,SDP %f"%(BTC,fee,trade_time,win,lose,win_rate,SDN,SDP))
 
-print("BTC %f fee %f trade time %d win %d lose %d,win rate %f"%(BTC,fee,trade_time,win,lose,win/trade_time *100))
-show(column(p))
+# show(column(p))
